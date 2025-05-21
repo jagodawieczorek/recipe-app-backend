e6eded7756f3ea213e009b2c1f0cb35916cc582f
Jagoda Wieczorek <jagoda.maria.wieczorek@gmail.com>
2025-05-21 17:17:50 +0200

    using gemma3:4b LLM, it seems it is a good choice to run a polish chat on a laptop

--- src/main/java/jagodawieczorek/recipeappbackend/config/OllamaConfig.java
+++ src/main/java/jagodawieczorek/recipeappbackend/config/OllamaConfig.java
@@ -1,17 +1,24 @@
 package jagodawieczorek.recipeappbackend.config;
 
 import dev.langchain4j.model.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OllamaConfig {
 
+    @Value("${ollama.base-url}")
+    private String baseUrl;
+
+    @Value("${ollama.model-name}")
+    private String modelName;
+
     @Bean
     public OllamaChatModel getOllamaChatModel() {
         return OllamaChatModel.builder()
-                .baseUrl("http://localhost:11434")
-                .modelName("llama3.2")
+                .baseUrl(baseUrl)
+                .modelName(modelName)
                 .logRequests(true)
                 .build();
     }
